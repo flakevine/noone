0a92ced506774592c18ed470b8fb489776fc02c2
viktormarinho <viktormpcs@gmail.com>
2023-01-25 21:23:48 -0300

    Start project

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+    entry: ["src/index.ts"],
+    sourcemap: true,
+    clean: true,
+    dts: true,
+    splitting: true,
+    bundle: true,
+    format: ["esm", "cjs"],  
+})
